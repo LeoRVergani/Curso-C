a7a4a1474eed368b448a2d8a500a8aa0adcab696
leorvergani <vergani@outlook.com.br>
2024-12-06 01:00:10 -0300

    first commit

---
+++ EX001 - Listagem.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main() {
+    printf("\t<<< EX001 - Listagem >>>");
+    printf("\n");
+    printf("\t    Listagem de Alunos \n");
+    printf("\n");
+    printf("\t    Nome \t     Nota\n");
+    printf("\t    --------------------- \n");
+    printf("\t    Ana Beatriz \t8.5\n");
+    printf("\t    Bianca Martins\t9.0\n");
+}
+
+// Comentário em C
+/* Comentário para várias linhas em
+C, testando... */
---
+++ ambiente.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+void main() {
+    printf("Oie, \n tudo \n bem?");
+}
